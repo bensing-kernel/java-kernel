f1112ba72196b3dcb2f9d94d4c585788e8f4d13d
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2022-11-18 23:00:21 -0500

    removed DisplayName(..) from NameTests

--- src/test/java/io/bensing/kernel/NameTests.java
+++ src/test/java/io/bensing/kernel/NameTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("io.bensing.kernel - Name")
 public class NameTests {
 
     @Test
