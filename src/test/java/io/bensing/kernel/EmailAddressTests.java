fa8c26c883013b4ffd5bc04666c17c540cd54f71
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2022-11-22 05:34:40 -0500

    removed display name for EmailAddressTests

--- src/test/java/io/bensing/kernel/EmailAddressTests.java
+++ src/test/java/io/bensing/kernel/EmailAddressTests.java
@@ -1,11 +1,9 @@
 package io.bensing.kernel;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Kernel - Value Object - Email Address")
 public class EmailAddressTests {
 
     @Test
